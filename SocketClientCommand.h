f4916d190905b8bda698e1edb049322ff8ec87c2
oracleloyall <873988212@qq.com>
2016-07-19 16:03:28 +0800

    include

---
+++ SocketClientCommand.h
@@ -0,0 +1,12 @@
+#ifndef _SOCKETCLIENTCOMMAND_H
+#define _SOCKETCLIENTCOMMAND_H
+
+#include "SocketClient.h"
+
+class SocketClientCommand {
+public:
+    virtual ~SocketClientCommand() { }
+    virtual void runSocketCommand(SocketClient *client) = 0;
+};
+
+#endif
